c9fd03242bd63e669c138d5c9b718c3989a42e0a
LucasGabrielAntonete <lucasantonete@gmail.com>
2024-11-27 20:36:47 -0300

    FEAT: Signal for credit

--- src/core/campus/infra/campus_django_app/signals.py
+++ src/core/campus/infra/campus_django_app/signals.py
@@ -1,10 +1,10 @@
-from core.credit.infra.credit_django_app.models import Credit
-from core.campus.infra.campus_django_app.models import Student
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+# from core.credit.infra.credit_django_app.models import Credit
+# from core.campus.infra.campus_django_app.models import Student
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
 
 
-@receiver(post_save, sender=Student)
-def create_student_credit(sender, instance, created, **kwargs):
-    if created:
-        Credit.objects.create(student=instance)
+# @receiver(post_save, sender=Student)
+# def create_student_credit(sender, instance, created, **kwargs):
+#     if created:
+#         Credit.objects.create(student=instance)
--- src/core/credit/infra/credit_django_app/apps.py
+++ src/core/credit/infra/credit_django_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CreditDjangoAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core.credit.infra.credit_django_app'
+
+    def ready(self):
+        import core.credit.infra.credit_django_app.signal
